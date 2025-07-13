f4d69eddf11d230db375d11025d5682bdec311e9
mdavid2 <98088727+mdavid2@users.noreply.github.com>
2025-07-13 09:48:26 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s http://localhost:3000/health | grep "Still working" && echo "PASS ✅" || echo "FAIL ❌"
+curl -s http://localhost:3000/health | grep "Still working" && echo "PASS ✅" || echo "FAIL ❌" 
