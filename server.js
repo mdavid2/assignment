c706a226ac9c5bb8d104848f02a1c8289e04d7c3
mdavid2 <98088727+mdavid2@users.noreply.github.com>
2025-07-16 10:08:52 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.get('/health', (req, res) => res.send('Still working... on *my* machine 🧃'));
 
+app.get('/status', (req, res) => {
+  res.json({ status: 'ok', timestamp: Date.now() });
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
